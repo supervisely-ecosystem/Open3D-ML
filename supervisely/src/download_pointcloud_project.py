1eece32211168ebac462d75ccf714babaf070efd
korvine <visignibraem@gmail.com>
2021-07-16 20:15:58 +0300

    download pointcloud project method found

---
+++ supervisely/src/download_pointcloud_project.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+import supervisely_lib as sly
+from supervisely_lib.project.pointcloud_project import download_pointcloud_project
+
+if __name__ == "__main__":
+    dest_dir = '/data/sly_project'
+    shutil.rmtree(dest_dir)  # WARNING!
+    project_id = 5423
+    api = sly.Api.from_env()
+
+    download_pointcloud_project(api, project_id, dest_dir, dataset_ids=None, download_items=True, log_progress=True)
+
+    sly.logger.info('PROJECT_DOWNLOADED', extra={'dest_dir': dest_dir,
+                                                 'datasets': [x for x in os.listdir(dest_dir) if 'json' not in x]})
