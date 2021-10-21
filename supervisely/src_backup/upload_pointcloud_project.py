a829f0656150a1f4701025442d3dd7a110ee7070
kashkarov <kashkarov@dedicorp.ru>
2021-10-21 19:35:34 +0300

    New lyft converter

--- supervisely/src_backup/upload_pointcloud_project.py
+++ supervisely/src_backup/upload_pointcloud_project.py
@@ -9,9 +9,9 @@ from supervisely_lib.video_annotation.key_id_map import KeyIdMap
 if __name__ == "__main__":
     api = sly.Api.from_env()
     base_dir = '/data/'
-    project_name = "LyftSequence4_filtered"
+    project_name = "LyftPhotoContext"
     project_id = None
-    max_n = 11
+    max_n = 1000
     if project_id is not None:
         project = api.project.get_info_by_id(project_id)  # to existing project
     else:
@@ -31,15 +31,25 @@ if __name__ == "__main__":
         dataset = api.dataset.create(project.id, dataset_fs.name, change_name_if_conflict=True)
         sly.logger.info("dataset {!r} [id={!r}] has been created".format(dataset.name, dataset.id))
         i = 0
-        for item_name in tqdm.tqdm(dataset_fs):
+        names = [item_name for item_name in dataset_fs]
+        print(names)
+        for item_name in sorted(names):
 
             if i >= max_n:
                 break
             i += 1
 
             item_path, related_images_dir, ann_path = dataset_fs.get_item_paths(item_name)
+            related_items = dataset_fs.get_related_images(item_name)
 
-            item_meta = {}
+            try:
+                _, meta = related_items[0]
+                timestamp = meta[ApiField.META]['timestamp']
+                if timestamp:
+                    item_meta = {"timestamp": timestamp}
+            except (KeyError, IndexError):
+                item_meta = {}
+
             pointcloud = api.pointcloud.upload_path(dataset.id, item_name, item_path, item_meta)
 
             # validate_item_annotation
@@ -50,7 +60,6 @@ if __name__ == "__main__":
             api.pointcloud.annotation.append(pointcloud.id, ann, uploaded_objects)
 
             # upload related_images if exist
-            related_items = dataset_fs.get_related_images(item_name)
             if len(related_items) != 0:
                 rimg_infos = []
                 for img_path, meta_json in related_items:
